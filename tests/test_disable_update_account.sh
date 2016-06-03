de36e9c8e4ec296b78c8632a20402ac6f3c529b3
Danil Pismenny <danil@brandymint.ru>
2016-06-03 09:53:51 +0300

    uuid instead of id

--- tests/test_disable_update_account.sh
+++ tests/test_disable_update_account.sh
@@ -5,13 +5,13 @@ echo "Можно обновлять детали"
 . ./tests/init.sh && \
 . ./tests/2accounts.sh && \
 
-./tests/assert_result.sh "delete from OPENBILL_ACCOUNTS  where id=1" 'ERROR:  Cannot delete account' && \
+./tests/assert_result.sh "delete from OPENBILL_ACCOUNTS" 'ERROR:  Cannot delete account' && \
 
 # TODO: Пока эти тесты еще не проходят
 
 # Это можно
-echo "PENDING ""update OPENBILL_ACCOUNTS  set details='some' where id=1" 'UPDATE 1' && \
+echo "PENDING ""update OPENBILL_ACCOUNTS set details='some' where id=$ACCOUNT1_UUID" 'UPDATE 1' && \
 
 # Нельзя этому случиться
-echo "PENDING " ./tests/assert_result.sh "update OPENBILL_ACCOUNTS set amount_cents=123 where id=1" 'ERROR:  Cannot directly update amount_cents and timestamps of account' && \
-echo "PENDING " ./tests/assert_result.sh "update OPENBILL_ACCOUNTS set created_at=current_date where id=1" 'ERROR:  Cannot directly update amount_cents and timestamps of account'
+echo "PENDING " ./tests/assert_result.sh "update OPENBILL_ACCOUNTS set amount_cents=123 where id=$ACCOUNT1_UUID" 'ERROR:  Cannot directly update amount_cents and timestamps of account' && \
+echo "PENDING " ./tests/assert_result.sh "update OPENBILL_ACCOUNTS set created_at=current_date where id=$ACCOUNT1_UUID" 'ERROR:  Cannot directly update amount_cents and timestamps of account'
