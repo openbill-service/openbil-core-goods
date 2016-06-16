baebc440b65bffd6344f91423557e2036a05925e
Danil Pismenny <danil@brandymint.ru>
2016-06-16 13:21:41 +0300

    Добавил owner_id

--- sql/4_migration_add_goods.sql
+++ sql/4_migration_add_goods.sql
@@ -1,4 +1,5 @@
 CREATE                TABLE OPENBILL_GOODS (
+  owner_id            UUID,
   id                  UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   group_id            uuid,
   title               varchar(255) not null,
@@ -14,6 +15,7 @@ CREATE UNIQUE INDEX index_goods_on_group_id ON OPENBILL_GOODS USING btree (group
 CREATE INDEX index_goods_on_meta ON OPENBILL_GOODS USING gin (meta);
 
 CREATE                TABLE OPENBILL_GOODS_AVAILABILITIES (
+  owner_id            UUID,
   id                  UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   account_id          uuid not null,
   good_id             uuid not null,
@@ -42,8 +44,9 @@ ALTER TABLE OPENBILL_TRANSACTIONS
 
 CREATE OR REPLACE FUNCTION process_account_transaction_with_good() RETURNS TRIGGER AS $process_transaction$
 BEGIN
-  IF NEW.good_id IS NULL THEN
-    RETURN NEW;
+  PERFORM id FROM OPENBILL_GOODS where (owner_id = NEW.owner_id OR (NEW.owner_id is NULL and owner_id is NULL));
+  IF NOT FOUND THEN
+    RAISE EXCEPTION 'No such good in this owner (#%)', NEW.owner_id;
   END IF;
 
   PERFORM id FROM OPENBILL_GOODS where id = NEW.good_id and unit = NEW.good_unit;
@@ -51,12 +54,12 @@ BEGIN
     RAISE EXCEPTION 'Good (#%) has wrong good unit (#%)', NEW.good_id, NEW.good_unit;
   END IF;
 
-  INSERT INTO OPENBILL_GOODS_AVAILABILITIES AS GA (account_id, good_id, value)
-    VALUES (NEW.from_account_id, NEW.good_id, - NEW.good_value)
+  INSERT INTO OPENBILL_GOODS_AVAILABILITIES AS GA (owner_id, account_id, good_id, value)
+    VALUES (NEW.owner_id, NEW.from_account_id, NEW.good_id, - NEW.good_value)
     ON CONFLICT (account_id, good_id) DO UPDATE SET value = GA.value - NEW.good_value;
 
-  INSERT INTO OPENBILL_GOODS_AVAILABILITIES AS GA (account_id, good_id, value)
-    VALUES (NEW.to_account_id, NEW.good_id, NEW.good_value)
+  INSERT INTO OPENBILL_GOODS_AVAILABILITIES AS GA (owner_id, account_id, good_id, value)
+    VALUES (NEW.owner_id, NEW.to_account_id, NEW.good_id, NEW.good_value)
     ON CONFLICT (account_id, good_id) DO UPDATE SET value = GA.value + NEW.good_value;
 
   return NEW;
@@ -65,5 +68,4 @@ END
 $process_transaction$ LANGUAGE plpgsql;
 
 CREATE TRIGGER process_account_transaction_with_good
-  AFTER INSERT ON OPENBILL_TRANSACTIONS FOR EACH ROW EXECUTE PROCEDURE process_account_transaction_with_good();
-
+  AFTER INSERT ON OPENBILL_TRANSACTIONS FOR EACH ROW WHEN (NEW.good_id IS NOT NULL) EXECUTE PROCEDURE process_account_transaction_with_good();
