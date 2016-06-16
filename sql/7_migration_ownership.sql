ea3100785e5bf1670b4904c5886b654746fe9954
Danil Pismenny <danil@brandymint.ru>
2016-06-16 13:37:46 +0300

    fix ownership

--- sql/7_migration_ownership.sql
+++ sql/7_migration_ownership.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE FUNCTION constraint_transaction_ownership() RETURNS TRIGGER AS $process_transaction$
 BEGIN
-  PERFORM * FROM OPENBILL_ACCOUNTS WHERE id = NEW.from_account_id and owner_id = NEW.owner_id;
+  PERFORM id FROM OPENBILL_ACCOUNTS WHERE id = NEW.from_account_id and (owner_id = NEW.owner_id OR (owner_id is NULL and NEW.owner_id is NULL) );
   IF NOT FOUND THEN
     RAISE EXCEPTION 'No such source account in this owner (to #%)', NEW.owner_id;
   END IF;
 
-  PEFFORM * FROM OPENBILL_ACCOUNTS WHERE id = NEW.to_account_id and owner_id = NEW.owner_id;
+  PERFORM id FROM OPENBILL_ACCOUNTS WHERE id = NEW.to_account_id and (owner_id = NEW.owner_id OR (owner_id is NULL and NEW.owner_id is NULL) );
   IF NOT FOUND THEN
     RAISE EXCEPTION 'No such destination account in this owner (to #%)', NEW.owner_id;
   END IF;
