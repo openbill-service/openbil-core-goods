de853120015845cebbfb75b8cd59bc44fdb94975
Andrei Sidorov <vozdelaluz@gmail.com>
2016-07-11 15:03:24 +0300

    Add operations

--- sql/0_db.sql
+++ sql/0_db.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public; 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 CREATE                TABLE OPENBILL_CATEGORIES (
   owner_id            UUID,
@@ -36,19 +36,32 @@ CREATE UNIQUE INDEX index_accounts_on_key ON OPENBILL_ACCOUNTS USING btree (key)
 CREATE INDEX index_accounts_on_meta ON OPENBILL_ACCOUNTS USING gin (meta);
 CREATE INDEX index_accounts_on_created_at ON OPENBILL_ACCOUNTS USING btree (created_at);
 
-CREATE TABLE OPENBILL_TRANSACTIONS (
-  owner_id            UUID,
+CREATE TABLE OPENBILL_OPERATIONS (
   id              UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  username        character varying(255) not null,
   created_at      timestamp without time zone default current_timestamp,
   from_account_id uuid not null,
   to_account_id   uuid not null,
+  owner_id        UUID,
+  key             character varying(256) not null,
+  details         text not null,
+  meta            hstore not null default ''::hstore
+);
+
+CREATE TABLE OPENBILL_TRANSACTIONS (
+  owner_id            UUID,
+  id              UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  operation_id    UUID,
+  username        character varying(255) not null,
+  created_at      timestamp without time zone default current_timestamp,
+  from_account_id uuid not null,
+  to_account_id   uuid not null CONSTRAINT different_accounts CHECK (to_account_id<>from_account_id),
   amount_cents    numeric not null CONSTRAINT positive CHECK (amount_cents>0),
   amount_currency char(3) not null,
   key             character varying(256) not null,
   details         text not null,
   meta            hstore not null default ''::hstore,
   foreign key (from_account_id) REFERENCES OPENBILL_ACCOUNTS (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  foreign key (operation_id) REFERENCES OPENBILL_OPERATIONS (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
   foreign key (to_account_id) REFERENCES OPENBILL_ACCOUNTS (id)
 );
 
